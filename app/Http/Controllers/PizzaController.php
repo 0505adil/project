76c5bc0e6dbecc93e67dd532cfd8e9f16230c018
adilet <0505adil00@gmail.com>
2020-04-27 22:08:14 +0600

    Update PizzaController.php

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -7,10 +7,29 @@ use DB;
 
 class PizzaController extends Controller
 {
-    public function index()
+    public function getData()
     {
         $pizza = DB::table('pizza')->get();
-
-        return view('pizza', ['pizza' => $pizza]);
+		$pizza_type = DB::table('pizza_type')->get();
+        return view('pizza', compact('pizza', 'pizza_type'));
     }
+
+    public function insert(Request $request) {
+    	$size = $request->input('size');
+    	$name = $request->input('pizzaName');
+
+		$pizza = DB::table('pizza')->get();
+		$pizza_type = DB::table('pizza_type')->get();
+    	foreach ($pizza_type as $key => $value) {
+    		if($size == $value->size) {
+    			$price = $value->price;
+    		}
+    	}
+
+    	$data=array('name'=>$name,"size"=>$size,"price"=>$price,"created_at" =>  \Carbon\Carbon::now(),
+            "updated_at" => \Carbon\Carbon::now(),);
+    	DB::table('order')->insert($data);
+    }
+    
+
 }
