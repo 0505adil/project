c628522f6faae692cf51d96ffdb7575ecb348f53
adilet <0505adil00@gmail.com>
2020-04-27 22:08:28 +0600

    Update header.blade.php

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -12,11 +12,10 @@
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#"><img src="img/icons/outline_local.png" class="icon ">   Pizza</a>
-      </li>
+      </li>                
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-    </form>
+    <form method="get" action="/basket">
+      <button class="btn btn-success">Корзина</button>
+    </form>    
   </div>
 </nav>
