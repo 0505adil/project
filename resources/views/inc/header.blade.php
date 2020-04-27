b687ce12675d189d53e05b2dbc344bd5a2e279be
adilet <0505adil00@gmail.com>
2020-04-27 12:05:30 +0600

    Initial commit

---
+++ resources/views/inc/header.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+  <a class="navbar-brand" href="#">PizzaKz</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item ml-3 mr-3">
+        
+        <a class="nav-link" href="#"><img src="img/icons/outline_home.png" class="icon ">   Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><img src="img/icons/outline_local.png" class="icon ">   Pizza</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
