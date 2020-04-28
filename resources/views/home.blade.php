7c34c9d1979a09d4ef7bc7ddaadcf6313aa01036
Dias73 <60936420+Dias73@users.noreply.github.com>
2020-04-28 22:13:46 +0600

    home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,44 @@
 @extends ('layouts.app')
 
 @section('content')
-
+    <div class="container-fluid sl">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/home/1.png" class="d-block w-100 a"  alt="PizzaKz1">
+            </div>
+            <div class="carousel-item">
+                <img src="img/home/2.png" class="d-block w-100" alt="PizzaKz2">
+            </div>
+            <div class="carousel-item">
+                <img src="img/home/3.png" class="d-block w-100" alt="PizzaKz3">
+            </div>
+            <div class="carousel-item">
+                <img src="img/home/4.png" class="d-block w-100" alt="PizzaKz4">
+            </div>
+            <div class="carousel-item">
+                <img src="img/home/5.png" class="d-block w-100" alt="PizzaKz4">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/home.js"></script>
 @endsection
