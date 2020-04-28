7024a5ded345573d95a11b45d0ac3a07a5d5de88
adilet <0505adil00@gmail.com>
2020-04-28 16:36:24 +0600

    Update PizzaController.php

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -26,10 +26,43 @@ class PizzaController extends Controller
     		}
     	}
 
-    	$data=array('name'=>$name,"size"=>$size,"price"=>$price,"created_at" =>  \Carbon\Carbon::now(),
-            "updated_at" => \Carbon\Carbon::now(),);
+    	$data=array('name'=>$name,"size"=>$size,"price"=>$price, 'amount'=> 1, "created_at" =>  \Carbon\Carbon::now(), "updated_at" => \Carbon\Carbon::now(),);
+    	$count = DB::table('order')->where('name', $name)->where('size', $size)->count();
+    	if($count > 0) {
+    		return redirect()->back()->with('alert', 'Уже в корзине!');
+    	}
     	DB::table('order')->insert($data);
+    	return redirect()->back();	
+    }
+
+    public function getOrders() {
+    	$order = DB::table('order')->get();
+
+    	return view('basket', compact('order'));
+    }
+
+    public function inc($id) {
+    	DB::table('order')
+            ->where('id', $id)
+            ->increment('amount');
+
+        return redirect()->back();
     }
     
+    public function dec($id) {
+    	
+    	DB::table('order')
+            ->where('id', $id)
+            ->decrement('amount');
+        
+        DB::table('order')
+            ->where('id', $id)
+            ->where('amount', '<=', 0)
+            ->delete();
+
+          
+
+        return redirect()->back();
+    }
 
 }
