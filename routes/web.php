606eb4062a9fdf488d3a614d33a07c3385dfd2a7
adilet <0505adil00@gmail.com>
2020-04-28 16:36:37 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,12 @@ Route::get('/pizza','PizzaController@getData');
 
 Route::get('/pizza/get','PizzaController@insert');
 
+Route::get('/basket', function () {
+    return view('basket');
+});
+
+Route::get('/basket','PizzaController@getOrders');
+
+Route::get('/basket/inc/{id}', 'PizzaController@inc')->name('basket/inc');
+
+Route::get('/basket/dec/{id}', 'PizzaController@dec')->name('basket/dec');
