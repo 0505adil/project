67e4635c58210d444f6fa15d37b5373f10bd7057
Bolatov Dias <60469781+disaPrince@users.noreply.github.com>
2020-04-29 22:38:07 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
+Route::get('/registr', function () {
+    return view('registr');
+});
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::post('/login/submit','LoginController@submit');
+
+Route::post('/registr/submit','RegistrController@registr');
 
 Route::get('/pizza', function () {
     return view('pizza');
