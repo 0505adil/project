ea6c145f21268aebeff506a64356ee278c27b0b4
adilet <0505adil00@gmail.com>
2020-04-29 18:43:53 +0600

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -19346,9 +19346,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Арзу\AppData\Roaming\Composer\project\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\Users\Арзу\AppData\Roaming\Composer\project\resources\sass\app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! C:\Users\Арзу\AppData\Roaming\Composer\project\resources\sass\home.scss */"./resources/sass/home.scss");
+__webpack_require__(/*! C:\Users\adile\Documents\laravel\project\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\Users\adile\Documents\laravel\project\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! C:\Users\adile\Documents\laravel\project\resources\sass\home.scss */"./resources/sass/home.scss");
 
 
 /***/ })
