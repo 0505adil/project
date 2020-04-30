c5f3f7b05c9a76e23a3f90e0ca62274f450f0012
adilet <0505adil00@gmail.com>
2020-04-30 13:25:09 +0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/app.css">
     <link rel="stylesheet" type="text/css" href="css/home.css">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
 	@include('inc.header')
