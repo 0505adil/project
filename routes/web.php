659b0c152d1d54caf492b287a3d83b8bae1e6f61
adilet <0505adil00@gmail.com>
2020-04-30 13:25:19 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+	Auth::logout();
     return view('home');
 })->name('home');
 
 Route::get('/registr', function () {
-    return view('registr');
+    return view('Auth.registr');
 });
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
-Route::post('/login/submit','LoginController@submit');
-
-Route::post('/registr/submit','RegistrController@registr');
+Route::get('/log', function () {
+    return view('Auth.login');
+})->name('log');
 
 Route::get('/pizza', function () {
     return view('pizza');
@@ -25,12 +22,17 @@ Route::get('/pizza','PizzaController@getData');
 
 Route::get('/pizza/get','PizzaController@insert');
 
-Route::get('/basket', function () {
-    return view('basket');
+Auth::routes();
+
+Route::group( ['middleware' => 'auth', 'before' => 'auth' ],  function()
+{
+	   	Route::get('/basket', function () {
+	    return view('basket');
+	});
+
+	Route::get('/basket','PizzaController@getOrders');
+
+	Route::get('/basket/inc/{id}', 'PizzaController@inc')->name('basket/inc');
+
+	Route::get('/basket/dec/{id}', 'PizzaController@dec')->name('basket/dec');
 });
-
-Route::get('/basket','PizzaController@getOrders');
-
-Route::get('/basket/inc/{id}', 'PizzaController@inc')->name('basket/inc');
-
-Route::get('/basket/dec/{id}', 'PizzaController@dec')->name('basket/dec');
